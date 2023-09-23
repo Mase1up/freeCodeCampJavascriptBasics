682dc7302c25b56cb753499236d73f917cea6fcd
Mase1up <mason.l.keith@gmail.com>
2023-09-23 13:10:02 -0400

    Enter 'using objs for lookups' question

--- javascript_basics.js
+++ javascript_basics.js
@@ -103,3 +103,40 @@ const testObj = {
   const playerNumber = 16;  // Change this line
   const player = testObj[playerNumber];   // Change this line
 
+
+
+// Convert the switch statement into an object called lookup. 
+// Use it to look up val and assign the associated string to the result variable.
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/using-objects-for-lookups
+// Setup
+function phoneticLookup(val) {
+    let result = "";
+  
+    // Only change code below this line
+    switch(val) {
+      case "alpha":
+        result = "Adams";
+        break;
+      case "bravo":
+        result = "Boston";
+        break;
+      case "charlie":
+        result = "Chicago";
+        break;
+      case "delta":
+        result = "Denver";
+        break;
+      case "echo":
+        result = "Easy";
+        break;
+      case "foxtrot":
+        result = "Frank";
+    }
+
+
+  
+    // Only change code above this line
+    return result;
+  }
+  
+  phoneticLookup("charlie");
